239370deba72c5402906bb27fc8479e067c0c020
dusdn1702 <dusdn1702@naver.com>
2020-06-06 22:54:55 +0900

    11559.cpp_f

---
+++ baekjoon/11559.cpp
@@ -0,0 +1,25 @@
+//뿌요뿌요
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+#define M 6
+#define N 12
+char map[N][M];
+bool visit[N][M];
+
+void input(){
+    for(int i=0;i<N;i++){
+        for(int j=0;j<M;j++){
+            cin >> map[i][j];
+        }
+    }
+}
+void solution(){
+
+}
+int main(void){
+
+}
