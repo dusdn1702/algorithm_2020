aa1a1054d6758a7d7e7fa56445090fa73576f6b0
dusdn1702 <dusdn1702@naver.com>
2020-06-24 23:59:26 +0900

    소수찾기_1

--- programmers/priceNumber.cpp
+++ programmers/priceNumber.cpp
@@ -25,3 +25,23 @@ int solution(int n) {
     }
     return answer;
 }
+
+//배수이면 0을 주고 0아닌 거 찾는 방식
+int solution(int n) {
+    int answer = 0;
+    vector<int> arr(n);
+
+    for(int i=2;i<=n;i++){
+        arr[i]=i;
+    }
+    for(int i=2;i<=n;i++){
+        if(arr[i]==0)continue;
+        for(int j=i+i;j<=n;j+=i){
+            arr[j]=0;
+        }
+    }
+    for(int i=2;i<=n;i++){
+        if(arr[i]!=0) answer++;
+    }
+    return answer;
+}
