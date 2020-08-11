53e4a8ab33cf2d7666c30841155b0aad3b0ddde0
dusdn1702 <dusdn1702@naver.com>
2020-08-11 22:34:19 +0900

    기본 개발 환경 setting용

---
+++ baekjoon/test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+    string a;
+    cin >> a;
+    cout << a;
+}
--- python/tempCodeRunnerFile.py
+++ python/tempCodeRunnerFile.py
@@ -1,2 +1,19 @@
-    if A > 23:
-        A-=24
+#10162
+n = int(input())
+A=5*60
+B=60
+C=10
+a,b,c=0,0,0
+while n-A>=0:
+    n-=A
+    a+=1
+while n-B>=0:
+    n-=B
+    b+=1
+while n-C>=0:
+    n-=C
+    c+=1
+if n!=0:
+    print(-1)
+else:
+    print(a,b,c)
